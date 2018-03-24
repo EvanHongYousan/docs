688a02d6188a8f8b1717991d11ba4c12c71ee815
EvanHongYousan <1370204201@qq.com>
2018-03-24 11:13:07 +0800

    update doc

--- examples/decoratorTest.js
+++ examples/decoratorTest.js
@@ -1,31 +1,31 @@
-Function.prototype.before = function(beforefn){
+Function.prototype.before = function (beforefn) {
     var _self = this;
-    return function(){
+    return function () {
         beforefn.apply(this, arguments);
         return _self.apply(this, arguments);
     };
 };
 
-Function.prototype.after = function(afterfn){
+Function.prototype.after = function (afterfn) {
     var _self = this;
-    return function(){
+    return function () {
         var ret = _self.apply(this, arguments);;
         afterfn.apply(this, arguments);
         return ret;
     };
 };
 
-var func = function(param){
+var func = function (param) {
     console.log(param);
 }
 
-func = func.before(function(param){
+func = func.before(function (param) {
     param.kk = 'kk';
     console.log('decorator params');
 });
 
-func = func.after(function(param){
+func = func.after(function (param) {
     console.log('report: ' + JSON.stringify(param));
 });
 
-func({aw:'wa'});
+func({ aw: 'wa' });
